218e6e415c8504bfb560aaadf7c68a3a5c0c9b8a
wanto-production <ikwansatria3974@gmail.com>
2026-04-03 10:08:39 +0700

    update

--- app/app.vue
+++ app/app.vue
@@ -13,6 +13,10 @@ useHead({
       name: 'google-site-verification',
       content: 'bWRaoFVlyB345Wietszb9IvK4x9MIN9hvtEA9ChIgoQ',
     },
+    {
+      name: 'msvalidate.01',
+      content: '7C8C851EE3CFCAE1B9971B0ECAC9BEBF',
+    },
   ],
 });
 </script>
